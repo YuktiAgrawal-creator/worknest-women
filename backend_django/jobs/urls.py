cbf76bafe1a58c3c146207a999878621f4611d95
Yukti Agrawal <yuktiagrawal22@gmail.com>
2026-07-29 01:32:45 +0530

    Fix jobs urls: single route for GET/POST and other endpoints

--- backend_django/jobs/urls.py
+++ backend_django/jobs/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.get_jobs),
-    path('recommended', views.get_recommended),
-    path('my', views.get_my_jobs),
-    path('<int:id>', views.get_job),
-    path('', views.create_job),
-    path('<int:id>', views.update_job),
+    # Single route handles GET (list) and POST (create)
+    path('', views.jobs_list_create, name='jobs-list-create'),
+    path('recommended', views.get_recommended, name='jobs-recommended'),
+    path('my', views.get_my_jobs, name='jobs-my'),
+    path('<int:id>', views.get_job, name='jobs-detail'),
+    path('<int:id>/update', views.update_job, name='jobs-update'),
 ]
