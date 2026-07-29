ea5aa7294753a736c2da188295f6db09f0bf3f00
Yukti Agrawal <yuktiagrawal22@gmail.com>
2026-07-29 23:06:32 +0530

    Set default app config for accounts and move AppConfig to apps.py

--- backend_django/accounts/__init__.py
+++ backend_django/accounts/__init__.py
@@ -1,5 +1,1 @@
-from django.apps import AppConfig
-
-class AccountsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounts'
+default_app_config = 'accounts.apps.AccountsConfig'
